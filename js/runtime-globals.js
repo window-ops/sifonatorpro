825b4d4b2babf0e9090409e24ec71ec21dd40423
window-ops <admin@miculpionier.ro>
2026-04-15 19:04:41 +0000

    Update file runtime-globals.js

--- js/runtime-globals.js
+++ js/runtime-globals.js
@@ -18,8 +18,8 @@
     const ronFmt=new Intl.NumberFormat('ro-RO',{
       style:'currency',
       currency:'RON',
-      maximumFractionDigits:0,
-      useGrouping:false
+      maximumFractionDigits:0
+      // useGrouping:false
     });
     globalThis.fRON=function(n){return ronFmt.format(Number(n)||0);};
   }
